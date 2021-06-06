91049a5e586641d1529f0e96eeb029f3b9a96f63
dmitri <fritsler.dmitri@yandex.ru>
2021-06-06 08:51:14 +0500

    download_homework_12

--- geek.c
+++ geek.c
@@ -22,11 +22,58 @@ void fillIntRandom(int* array, int size, int border) {
         *(array + i) = rand() % border;
 }
 
+void clearIntArray(int* arr, const int size) {
+    for (int i = 0; i < size; ++i) {
+        arr[i] = 0;
+    }
+}
+
+int get2dInt(int** array, const int row, const int col) {
+    return *((*(array + row)) + col);
+
+}
+void set2dInt(int** array, const int row, const int col, int value) {
+    *((*(array + row)) + col) = value;
+}
+int** init2dIntArray(int** array, const int row, const int col) {
+    array = (int**) calloc(sizeof(int*), row);
+    for (int i = 0; i < row; ++i) {
+        *(array + i) = (int*) calloc(sizeof(int), col);
+    }
+    return array;
+}
+void print2dIntArray(int** array, const int row, const int col, int offset) {
+    char format[7];
+    sprintf(format, "%%%dd", offset);
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            printf(format, get2dInt(array, i, j));
+        }
+        printf("\n");
+    }
+}
+void fill2dIntArray(int** array, const int row, const int col) {
+    for (int i = 0; i < row; ++i) {
+        for (int j = 0; j < col; ++j) {
+            int value = ((i + 1) * (j + 1));
+            //int value = rand() % 100;
+            set2dInt(array, i, j, value);
+        }
+    }
+}
+void setLineValues(int** matrix, int cols, int row, ...) {
+    va_list args;
+    va_start(args, row);
+    for (int i = 0; i < cols; ++i) {
+        set2dInt(matrix, row, i, va_arg(args, int));
+    }
+    va_end(args);
+}
+
 void initOneLinkList(OneLinkList *lst) {
     lst->head = NULL;
     lst->size = 0;
 }
-
 void printOneLinkIntNode(OneLinkNode *n) {
     if (n == NULL) {
         printf("[]");
@@ -34,7 +81,6 @@ void printOneLinkIntNode(OneLinkNode *n) {
     }
     printf("[%d]", n->dat);
 }
-
 void printOneLinkIntList(OneLinkList *lst) {
     OneLinkNode *current = lst->head;
     if (current == NULL) {
@@ -45,9 +91,8 @@ void printOneLinkIntList(OneLinkList *lst) {
             current = current->next;
         } while (current != NULL);
     }
-    printf(" Size list: %d \n", lst->size);
+    printf(" Size: %d \n", lst->size);
 }
-
 boolean pushOneLinkStack(OneLinkList *stack, T value) {
     OneLinkNode *tmp = (OneLinkNode*) malloc(sizeof(OneLinkNode));
     if (tmp == NULL) {
@@ -61,7 +106,6 @@ boolean pushOneLinkStack(OneLinkList *stack, T value) {
     stack->size++;
     return true;
 }
-
 T popOneLinkStack(OneLinkList *stack) {
     if (stack->size == 0) {
         printf("Stack is empty \n");
@@ -80,7 +124,6 @@ void initTwoLinkList(TwoLinkList *lst) {
     lst->tail = NULL;
     lst->size = 0;
 }
-
 void printTwoLinkIntNode(TwoLinkNode *n) {
     if (n == NULL) {
         printf("[]");
@@ -88,7 +131,6 @@ void printTwoLinkIntNode(TwoLinkNode *n) {
     }
     printf("[%d]", n->dat);
 }
-
 void printTwoLinkIntList(TwoLinkList *lst) {
     TwoLinkNode *current = lst->head;
     if (current == NULL) {
@@ -101,7 +143,6 @@ void printTwoLinkIntList(TwoLinkList *lst) {
     }
     printf(" Size: %d \n", lst->size);
 }
-
 boolean TwoLinkEnqueue(TwoLinkList *queue, T value) {
     TwoLinkNode *tmp = (TwoLinkNode*) malloc(sizeof(TwoLinkNode));
     if (tmp == NULL) {
@@ -123,7 +164,6 @@ boolean TwoLinkEnqueue(TwoLinkList *queue, T value) {
     queue->size++;
     return true;
 }
-
 T TwoLinkDequeue(TwoLinkList *queue) {
     if (queue->size == 0) {
         printf("Queue is empty \n");
@@ -157,4 +197,119 @@ void printCyclicIntList(OneLinkList *lst) {
         } while (current != lst->head);
     }
     printf(" Size: %d \n", lst->size);
+}
+
+BinTreeIntNode* treeInsert(BinTreeIntNode *t, int data) {
+    BinTreeIntNode *newNode;
+    newNode = (BinTreeIntNode*) malloc(sizeof(BinTreeIntNode));
+    newNode->key = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    BinTreeIntNode *current = t;
+    BinTreeIntNode *parent = t;
+    if (t == NULL) {
+        t = newNode;
+    } else {
+        while (current->key != data) {
+            parent = current;
+            if (current->key > data) {
+                current = current->left;
+                if (current == NULL) {
+                    parent->left = newNode;
+                    return t;
+                }
+            } else {
+                current = current->right;
+                if (current == NULL) {
+                    parent->right = newNode;
+                    return t;
+                }
+            }
+        }
+    }
+    return t;
+}
+void printBinTree(BinTreeIntNode *root) {
+    if (root) {
+        printf("%d", root->key);
+        if (root->left || root->right) {
+            printf("(");
+            if (root->left)
+                printBinTree(root->left);
+            else
+                printf("NULL");
+            printf(",");
+            if (root->right)
+                printBinTree(root->right);
+            else
+                printf("NULL");
+            printf(")");
+        }
+    }
+}
+BinTreeIntNode* getSuccessor(BinTreeIntNode *node) {
+    BinTreeIntNode *current = node->right;
+    BinTreeIntNode *parent = node;
+    BinTreeIntNode *s = node;
+
+    while (current != NULL) {
+        parent = s;
+        s = current;
+        current = current->left;
+    }
+    if (s != node->right) {
+        parent->left = s->right;
+        s->right = node->right;
+    }
+    return s;
+}
+boolean treeNodeDelete(BinTreeIntNode *root, int key) {
+    BinTreeIntNode *current = root;
+    BinTreeIntNode *parent = root;
+    boolean isLeftChild = true;
+
+    while (current->key != key) {
+        parent = current;
+        if (key < current->key) {
+            current = current->left;
+            isLeftChild = true;
+        } else {
+            current = current->right;
+            isLeftChild = false;
+        }
+        if (current == NULL)
+            return false;
+    }
+
+    if (current->left == NULL && current->right == NULL) {
+        if (current == root)
+            root = NULL;
+        else if (isLeftChild)
+            parent->left = NULL;
+        else
+            parent->right = NULL;
+    }
+    else if (current->right == NULL) {
+        if (isLeftChild)
+            parent->left = current->left;
+        else
+            parent->right = current->left;
+    }
+    else if (current->left == NULL) {
+        if (isLeftChild)
+            parent->left = current->right;
+        else
+            parent->right = current->right;
+    }
+    else {
+        BinTreeIntNode *successor = getSuccessor(current);
+        if (current == root)
+            root = successor;
+        else if (isLeftChild)
+            parent->left = successor;
+        else
+            parent->right = successor;
+        successor->left = current->left;
+    }
+    return true;
 }
